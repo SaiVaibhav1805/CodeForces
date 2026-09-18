67d2c26e1d40b77c3feb573da61cf7700b506aab
vaibhav <saivaibhav1805@gmail.com>
2026-09-19 00:11:00 +0530

    Day 4

---
+++ loveusername.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int first = sc.nextInt();
+
+        int min = first;
+        int max = first;
+
+        int count = 0;
+
+        for(int i = 1; i < n; i++) {
+
+            int x = sc.nextInt();
+
+            if(x > max) {
+                max = x;
+                count++;
+            }
+
+            else if(x < min) {
+                min = x;
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
